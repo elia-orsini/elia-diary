009ebeeae01f6c2c714a8d8c848581008dd28447
Your name here <elia.orsini@hotmail.com>
2025-07-18 15:11:17 +0800

    chore: linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import { client } from "@/sanity/client";
 import { fetchOptions } from "@/constants/constants";
-import Image from "next/image";
-import { urlFor } from "@/sanity/urlFor";
 import ImageGridComponent from "@/components/ImageGridComponent";
 
 const IMAGES = `*[_type == "imagesArchive"][]`;
@@ -10,15 +8,10 @@ export default async function IndexPage() {
   const projects = await client.fetch<any[]>(IMAGES, {}, fetchOptions);
   const media = projects[0].imageGroups.flat();
 
-  const media2024 = media.filter(
-    (media: any) => media.date && new Date(media.date).getFullYear() === 2024
-  );
-  const media2025 = media.filter((media: any) => new Date(media.date).getFullYear() === 2025);
-
   return (
     <main className="flex h-full w-screen flex-col font-normal">
-      <p className="mx-auto mt-40 text-3xl font-bold italic">Elia's Diary</p>
-      <p className="mx-auto my-20 text-xl font-bold px-2 text-center">
+      <p className="mx-auto mt-40 text-3xl font-bold italic">Elia&apos;s Diary</p>
+      <p className="mx-auto my-20 px-2 text-center text-xl font-bold">
         Just a big collection of photos meant to be taken in <i>all together</i>.
       </p>
 

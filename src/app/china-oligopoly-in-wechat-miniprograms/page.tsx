df12b591b6342af3cbf6a6be03ed3e6dc762f706
Your name here <elia.orsini@hotmail.com>
2025-07-30 12:08:42 +0800

    fix: small fixes

--- src/app/china-oligopoly-in-wechat-miniprograms/page.tsx
+++ src/app/china-oligopoly-in-wechat-miniprograms/page.tsx
@@ -1,6 +1,7 @@
 import ExperienceAscii from "@/components/Exp";
 import Image from "next/image";
 import localFont from "next/font/local";
+import { Metadata } from "next";
 
 const TimesNewRoman = localFont({
   src: [
@@ -40,30 +41,34 @@ export default async function IndexPage() {
         lunch or a quick snack from completely different places, every Wechat Miniprogram just feels
         so familiar. The reason behind this strange familiarity happens to be motivated by an
         effective oligopoly of businesses controlling the entire field of{" "}
-        <i>Wechat Miniprogram Development</i>. This might not seem very surprising as every industry
-        in tech seems to have small amounts of companies controlling big shares of the market (see
-        internet browsers, social networks, delivery apps).
+        <i>Wechat Miniprogram Development</i>. This effective oligopoly seems to be mostly formed by
+        Youzan and Flipos. This might not seem very surprising as every industry in tech seems to
+        have small amounts of companies controlling big shares of the market (see internet browsers,
+        social networks, delivery apps).
         <br />
-        This is therefore certainly not a new phenomenon. However, there is a big distinction
-        between the two phenomenons I have just mentioned. The most apparent of which is that while
-        for internet browsers or social networks each major player is a consumer-facing brand with
-        its own design philosophy, colour palette and typography, when it comes to the oligopoly of
-        companies developing wechat miniprograms these are not brands consumers typically see or
-        even care about. These big players are in charge of developing and making design decisions
-        for thousands of other brands in every major consumer sector. What results from this is just
-        a monotomy of design choices which becomes repetitive and boring. Every brand just starts
-        looking exactly like the others.
+        <br />
+        This is therefore certainly not new. However, there is a big distinction between the two
+        phenomenons I have just mentioned. The most apparent of which is that while for internet
+        browsers or social networks each major player is a consumer-facing brand with its own design
+        philosophy, colour palette and typography, when it comes to the oligopoly of companies
+        developing wechat miniprograms,{" "}
+        <b>these are not brands consumers typically see or even care about</b>. These big players,
+        instead, are in charge of developing consumer-facing online stores for thousands of brands
+        in every major consumer sector. What results from this is just a monotomy of layouts and
+        design choices which becomes repetitive and familiar. With this, every brand starts looking
+        more and more like their competitors until it&apos;s hard to distinguish which one is which.
         <br />
         <br />
         All this can seem like a positive aspect overall. I mean if you think about it, a user
         interface that looks very similar between brands makes it really easy for users to use and
         interact with. And that&apos;s a really good point. Usability is definitely a very important
-        factor when it comes to consumer facing services. However, while we might be gaining a lot
-        in terms of usability, we are losing so much in terms of aesthetic differences which
-        ultimately makes us love our favourite restaurant, bar, café, art gallery.
+        factor when it comes to consumer facing services, especially the ones we use every single
+        day. However, while we might be gaining a lot in terms of usability, we are losing so much
+        in terms of aesthetic differences which ultimately makes us love our favourite restaurant,
+        bar, café, art gallery.
         <br />
         <br />
-        To better understand this, we can just take a look at a few examples of restaurants menus{" "}
+        To better understand this, we can just take a look at a few examples of restaurant menus{" "}
         <a
           href="https://www.nytimes.com/interactive/2024/01/22/dining/restaurant-menu-trends.html?unlocked_article_code=1.ik4.ZmWJ.OxYUZyaSgfkW"
           className="underline"
@@ -124,7 +129,7 @@ export default async function IndexPage() {
           className={`my-[7px] h-auto w-full transition-opacity duration-500`}
         />
         <Image
-          src={"/blog/wechat-miniprograms/poltergeist-front-@@-900@x2.webp"}
+          src={"/blog/wechat-miniprograms/amano-@@-900@x2.webp"}
           alt=""
           width={"0"}
           height={"0"}
@@ -179,6 +184,44 @@ export default async function IndexPage() {
         it comes to the layout, the philosophy, the vibe you want to convey is just absolutely
         endless. And I think even tho it&apos;s very different, it <i>usually</i> remains easy to
         interpret for the people dining at that place.
+        <br />
+        <br />
+        And now we can compare this variety of aesthetics with three examples of WeChat Miniprograms
+        of three distrinct cafes/bakeries:
+      </p>
+
+      <div className="my-6 columns-3 gap-[7px]">
+        <Image
+          src={"/blog/wechat-miniprograms/2/o1.png"}
+          alt=""
+          width={"0"}
+          height={"0"}
+          sizes="500px"
+          className={`h-auto w-full transition-opacity duration-500`}
+        />
+        <Image
+          src={"/blog/wechat-miniprograms/2/o2.png"}
+          alt=""
+          width={"0"}
+          height={"0"}
+          sizes="500px"
+          className={`my-[7px] h-auto w-full transition-opacity duration-500`}
+        />
+        <Image
+          src={"/blog/wechat-miniprograms/2/o4.png"}
+          alt=""
+          width={"0"}
+          height={"0"}
+          sizes="500px"
+          className={`my-[7px] h-auto w-full transition-opacity duration-500`}
+        />
+      </div>
+
+      <p className="my-6 text-xl">
+        This is a very clear example of what&apos;s happening. However, I also need to admit that
+        it&apos;s not like this all the time. If anything, most times the Wechat Miniprograms look
+        different. Are they <i>different enough</i>? Not sure about that. Granted it&apos;s a subtle
+        distinction but one still worth making in my opinion.
       </p>
 
       <div className="my-6 columns-2 gap-[7px] md:columns-3">
@@ -233,7 +276,7 @@ export default async function IndexPage() {
       </div>
 
       <p className="my-6 text-xl">
-        Don&apos;t get me wrong. The few Wechat Miniprograms developers are doing an amazing job at
+        Don&apos;t get me wrong. The Wechat Miniprograms developers are doing an amazing job at
         trying to differentiate between the various brands using the same services. But I think
         it&apos;s just not enough to make the user get attached to a completely <i>unique</i> UI
         that brands could have. This is really easy to notice when interacting with most websites on
@@ -292,46 +335,10 @@ export default async function IndexPage() {
       </div>
 
       <p className="my-6 text-xl">
-        The curious thing is that even though most websites might use templates, they are
-        idiosyncratic enough to express what their brand is about. Some websites might be not
-        perfect, I have to conced that, but they still represent a unique experience. Again, see
-        when ordering from completely different cafes/bakeries in China:
+        The curious thing is that even though most websites might use templates, they remain
+        idiosyncratic enough to express what their brand is about. Some websites might not be
+        perfect but they still represent a <i>unique enough</i> experience.
       </p>
-
-      <div className="my-6 columns-4 gap-[7px]">
-        <Image
-          src={"/blog/wechat-miniprograms/2/o1.png"}
-          alt=""
-          width={"0"}
-          height={"0"}
-          sizes="500px"
-          className={`h-auto w-full transition-opacity duration-500`}
-        />
-        <Image
-          src={"/blog/wechat-miniprograms/2/o2.png"}
-          alt=""
-          width={"0"}
-          height={"0"}
-          sizes="500px"
-          className={`my-[7px] h-auto w-full transition-opacity duration-500`}
-        />
-        <Image
-          src={"/blog/wechat-miniprograms/2/o4.png"}
-          alt=""
-          width={"0"}
-          height={"0"}
-          sizes="500px"
-          className={`my-[7px] h-auto w-full transition-opacity duration-500`}
-        />
-        <Image
-          src={"/blog/wechat-miniprograms/2/o3.png"}
-          alt=""
-          width={"0"}
-          height={"0"}
-          sizes="500px"
-          className={`my-[7px] h-auto w-full transition-opacity duration-500`}
-        />
-      </div>
 
       <p className={`mx-auto mt-40 text-base font-bold text-red-600 ${TimesNewRoman.className}`}>
         It seems like you reached the end. <i>Thank you</i>.
@@ -341,3 +348,25 @@ export default async function IndexPage() {
     </main>
   );
 }
+
+export const metadata: Metadata = {
+  title: "China's Oligopoly in the world of WeChat Miniprograms | Elia's Diary",
+  description: "...",
+  icons: {
+    icon: "/globe.svg",
+  },
+  openGraph: {
+    title: "China's Oligopoly in the world of WeChat Miniprograms | Elia's Diary",
+    description: "...",
+    url: "https://diary.elia-orsini.com",
+    siteName: "Elia's Diary",
+    images: [
+      {
+        url: "/cover.png",
+        width: 500,
+        height: 500,
+        alt: "Elia's Diary",
+      },
+    ],
+  },
+};
